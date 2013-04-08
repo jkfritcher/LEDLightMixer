ec99fc710953bc1b026f1a5688a0fdabc9ee404a
Jason Fritcher <jkf@wolfnet.org>
2013-04-07 22:10:44 -0700

    Changed raw array data to structure to contain led states.

--- status.c
+++ status.c
@@ -3,11 +3,17 @@
 #include "time.h"
 #include "status.h"
 
+typedef struct {
+    uint8_t total_states;
+    uint8_t current_state;
+    uint16_t states[6];
+} led_state_t;
+
 static volatile STATUS status;
 
-static uint16_t LED_STATUS[][8] = {
-    { 6, 0, 250, 250, 500, 500, 750, 750 },
-    { 2, 0, 100, 2900, 0 },
+static led_state_t LED_STATE[3] = {
+    { 6, 0, { 250, 250, 500, 500, 750, 750 } },
+    { 2, 0, { 100, 2900, 0 } },
     { 0 }
 };
 
@@ -24,10 +30,10 @@ void status_led_update(millis_t now)
 {
     millis_t diff = time_difference(now, last_change);
 
-    uint8_t i = LED_STATUS[status][1];
-    if (diff >= LED_STATUS[status][2 + i]) {
+    uint8_t i = LED_STATE[status].current_state;
+    if (diff >= LED_STATE[status].states[i]) {
         PORTE ^= (1 << PORTE6);
-        LED_STATUS[status][1] = ((i + 1) % LED_STATUS[status][0]);        
+        LED_STATE[status].current_state = ((i + 1) % LED_STATE[status].total_states);
         last_change = now;
     }
 }
@@ -35,7 +41,7 @@ void status_led_update(millis_t now)
 void status_led_change_status(STATUS s)
 {
     if (s != status) {
-        LED_STATUS[s][1] = 0;
+        LED_STATE[s].current_state = 0;
         status = s;
         last_change = millis_get();
     }

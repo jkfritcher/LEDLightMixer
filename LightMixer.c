70811bce8974dcef4c90d0a5d34e188c155f849c
Jason Fritcher <jkf@wolfnet.org>
2013-04-14 23:27:21 -0700

    Added code to handle external switch to toggle LED. Added code to configure unused pins as inputs with pull-ups enabled. Changed LED output to phase correct PWM so that they'll actuall turn off with a 0 setting.

--- LightMixer.c
+++ LightMixer.c
@@ -67,6 +67,16 @@ int main(void)
         now = millis_get();
         status_led_update(now);
 
+        if ((PINB & (1 << PORTB4)) != 0) {
+            OCR1A = 0; /* blue */
+            OCR1B = 0; /* green */
+            OCR1C = 0; /* red */
+        } else {
+            OCR1A = 127; /* blue */
+            OCR1B = 127; /* green */
+            OCR1C = 127; /* red */
+        }
+
         /* Must throw away unused bytes from the host, or it will lock up while waiting for the device */
         CDC_Device_ReceiveByte(&LightMixer_CDC_Interface);
 
@@ -93,15 +103,33 @@ void SetupHardware(void)
 
     /* Setup Timer 1A-C for PWM mode */
     DDRB |= (1 << DDB7) | (1 << DDB6) | (1 << DDB5); /* Set B5-7 as outputs */
-    /* PWM, Fast, 8-bit, CLK_IO / 8, PWM @ 488.28 Hz */
+    PORTB &= ~((1 << PORTB7) | (1 << PORTB6) | (1 << PORTB5)); /* Disable pull-ups */
+    /* PWM, Phase correct, 8-bit, CLK_IO / 8, PWM @ 245.1 Hz */
+    TCCR1B = (1 << CS11);
     TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << COM1C1) | (1 << WGM10);
-    TCCR1B = (1 << WGM12) | (1 << CS11);
     OCR1A = 0; /* blue */
     OCR1B = 0; /* green */
     OCR1C = 0; /* red */
 
+    /* Init switch input */
+    DDRB &= ~(1 << DDB4); /* Set B4 as input */
+    PORTB |= (1 << PORTB4); /* Enable pull-up */
+
     /* Hardware Initialization */
     USB_Init();
+
+    /* Configure unused pins as inputs with a pull-up */
+    DDRB &= ~((1 << DDB3) | (1 << DDB2) | (1 << DDB1) | (1 << DDB0));
+    PORTB |= (1 << PORTB3) | (1 << PORTB2) | (1 << PORTB1) | (1 << PORTB0);
+    DDRC = 0;
+    PORTC = (1 << PORTC7) | (1 << PORTC6);
+    DDRD &= ~((1 << DDD7) | (1 << DDD6) | (1 << DDD5) |
+              (1 << DDD4) | (1 << DDD1) | (1 << DDD0));
+    PORTD |= (1 << PORTD7) | (1 << PORTD6) | (1 << PORTD5) |
+             (1 << PORTD4) | (1 << PORTD1) | (1 << PORTD0);
+    DDRF = 0;
+    PORTF = (1 << PORTF7) | (1 << PORTF6) | (1 << PORTF5) |
+            (1 << PORTF4) | (1 << PORTF1) | (1 << PORTF0);
 }
 
 /** Event handler for the library USB Connection event. */
